ead8d7fa86f2a1872c90aa5f623c8c3b6a0ccf5c
二货机器人 <smith3816@gmail.com>
2025-03-28 15:56:54 +0800

    chore: adjust config

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,9 +3,13 @@ import { defineConfig } from 'father';
 export default defineConfig({
   plugins: ['@rc-component/father-plugin'],
   esm: {
+    ignores: ['**/demo/**', '**/__tests/**'],
     input: 'components/',
+    parallel: true,
   },
   cjs: {
+    ignores: ['**/demo/**', '**/__tests/**'],
     input: 'components/',
+    parallel: true,
   },
 });
